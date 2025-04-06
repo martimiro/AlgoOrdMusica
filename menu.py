ce4698d5c44ea3e7371f2145d9fd1a26c9946c5b
mrt <mmirobar17@alumnes.ub.edu>
2025-04-06 12:18:06 +0200

    falta arreglar menu

--- menu.py
+++ menu.py
@@ -5,36 +5,54 @@ class Menu:
     def menu(self):
         entrada = 0
 
-        while(entrada == 0 or entrada > 2 or entrada < 0):
-            try:
-                print("== MENU ==")
-                print("[1] Introdueix la seva sèrie.")
-                print("[2] Genera una sèrie aletoria.")
+        try:
+            while(entrada == 0 or entrada > 3 or entrada < 0):
+                    print("============ MENÚ ==============")
+                    print("[1] Introdueix la seva sèrie.")
+                    print("[2] Genera una sèrie aletoria.")
+                    print("[3] Sortitr.")
+                    print("================================")
 
-                entrada = input("Escull una opció: ")
-                entrada = int(entrada)
+                    entrada = input("Escull una opció: ")
+                    entrada = int(entrada)
 
-                # Cas entrada incorrecta
-                if(entrada != 1 and entrada != 2):
-                    print("ERROR: L'entrada no és correcta.")
-                # Cas entrada = 1
-                elif(entrada == 1):
-                    print("En construcció")
-                # Cas entrada = 2
-                else:
-                    # Creem objecte llista
-                    serie = Serie()
-                    # Generem objecte llista                                        
-                    llista = serie.generarLlista()                                      
+                    # Cas entrada incorrecta
+                    if(entrada != 1 and entrada != 2):
+                        print("ERROR: L'entrada no és correcta.")
+                    # Cas entrada = 1
+                    elif(entrada == 1):
+                        print("En construcció")
+                    elif(entrada == 3):
+                        return
+                    # Cas entrada = 2
+                    else:
+                        # Creem objecte llista
+                        serie = Serie()
+                        # Generem objecte llista                                        
+                        llista = serie.generarLlista()
+                        print("La seria dodecafonica aleatoria és: ")
+                        print(str(llista)+"\n")
+        except ValueError:
+            print("ERROR: L'entrada no és correcta.")
+            entrada = 0
+        
+        entrada = 0
+        try:    
+            while(entrada == 0 or entrada < 0 or entrada > 1):                        
+                    print("============ MENÚ ==============")
+                    print("[1] Bubble Sort.")
+                    print("================================")
 
-                    print("La seria dodecafonica aleatoria és: ")
-                    print(str(llista)+"\n")
+                    if(entrada != 1):
+                        print("ERROR: L'entrada no és correcta.")
+                    elif(entrada == 1):
+                        bubble = BubbleSort()
+                        print("Llista ordenada: \n")
+                        llista_ord = bubble.bubble_sort(llista)
+                        print(llista_ord)
+        except ValueError:
+            print("ERROR: L'entrada no és correcta.")
+            entrada = 0
 
-                    bubble = BubbleSort()
-                    print("Llista ordenada: \n")
-                    llista_ord = bubble.bubble_sort(llista)
 
-
-            except ValueError:
-                print("ERROR: L'entrada no és correcta.")
-                entrada = 0
+            
