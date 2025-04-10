6220a3f0c652856a9f111350544cdd0b7986596f
mrt <mmirobar17@alumnes.ub.edu>
2025-04-10 20:17:16 +0200

    update bts

--- Model/algorismes/binaryTreeSort.py
+++ Model/algorismes/binaryTreeSort.py
@@ -2,25 +2,22 @@ from AlgoOrdMusica.Model.node import Node
 
 '''
 BUBBLE SORT
-COMPLEXITAT: O(n²)
+COMPLEXITAT: O(n²) no balancejat || O(n) balancejat
 '''
 
-class BinaryTreeSort:
-    def inorder(root, res):
-    # Recursive travesal 
-    if root:
-        inorder(root.left,res)
-        res.append(root.val)
-        inorder(root.right,res)
-
-    def treesort(arr):
-        # Build BST
-        if len(arr) == 0:
-            return arr
-        root = node(arr[0])
-        for i in range(1,len(arr)):
-            root.insert(arr[i])
-        # Traverse BST in order. 
-        res = []
-        inorder(root,res)
-        return res
+class SortTree:
+  def __init__(self, value):
+    self.left = None
+    self.value = value
+    self.right = None
+  def insert_val(self, _value):
+    if _value < self.value:
+       if self.left is None:
+           self.left = SortTree(_value)
+       else:
+           self.left.insert_val(_value)
+    else:
+       if self.right is None:
+          self.right = SortTree(_value)
+       else:
+          self.right.insert_val(_value)
--- README.md
+++ README.md
@@ -8,6 +8,6 @@
 Recorre l'array valor per valor. Cada un d'aquests el compara amb el valor següent. En el cas que aquest últim sigui més petit que el primer, s'intercanvien posicions de manera que el més petit va al davant i el més gran es queda darrera. Aplica aquest procediemnt tantes vegades com sigui necessari per acabar d'ordenar tota la llista.
 
 ### Binnary Tree Short
-#### *Complexitat: O(n²) no balancejat o O(n log n) balancejat*
+#### *Complexitat: O(n²) no balancejat || O(n log n) balancejat*
 
 Començem amb un arbre buit. Cada element de l'array es va inserint en un Binary Search Tree (BST). Quan l'arbre ja esta acabat, fem un inordre per obtenir la llista amb els valors ordenats.
