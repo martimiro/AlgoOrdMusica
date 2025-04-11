f2b3dc2d442e0b18da1f63950b808e7e3f71d4f4
mrt <mmirobar17@alumnes.ub.edu>
2025-04-11 11:35:29 +0200

    bts acabat

--- Model/binaryTreeSort.py
+++ Model/binaryTreeSort.py
@@ -8,7 +8,6 @@ class SortTree:
     self.left = None
     self.value = value
     self.right = None
-
   def insert_val(self, _value):
     if _value < self.value:
        if self.left is None:
@@ -20,7 +19,15 @@ class SortTree:
           self.right = SortTree(_value)
        else:
           self.right.insert_val(_value)
+  @classmethod
+  def display(cls, _node):
+     return list(filter(None, [i for b in [cls.display(_node.left) if isinstance(_node.left, SortTree) else [getattr(_node.left, 'value', None)], [_node.value], cls.display(_node.right) if 
+                                           isinstance(_node.right, SortTree) else [getattr(_node.right, 'value', None)]] for i in b]))
 
+"""
 tree = SortTree(4)
 for i in [5, 3, 1, 2, 8, 7, 4]:
   tree.insert_val(i)
+
+print(SortTree.display(tree))
+"""
